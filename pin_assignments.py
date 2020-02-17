7f06444b355d911a4d20035a29ce8f0e725cef7a
chris-oberdorf <cao5292@psu.edu>
2020-02-17 12:47:33 -0500

    Add pin assignments

---
+++ pin_assignments.py
@@ -0,0 +1,52 @@
+"""
+Team 2
+Shower Scrubber
+Pin assignments kept immutable for the sanity of the
+program (and the programmer!)
+"""
+
+# Start button: Input
+BTN_START = 2
+
+# Pumps: Output
+PUMP_WATER = 3
+PUMP_SOAP = 4
+
+# Solenoids: Output
+SOL_WATER = 14
+SOL_SOAP = 15
+
+# Limit switches: Input
+LSW_ONE = 17
+LSW_TWO = 18
+LSW_THREE = 27
+LSW_FOUR = 22
+LSW_FIVE = 23
+LSW_SIX = 24
+LSW_SEVEN = 10
+LSW_EIGHT = 9
+
+# Brush motors: Output
+BRUSH_ONE = 25
+BRUSH_TWO = 11
+
+# Wheel 1: PWM and output
+LEFT_PWM = 20
+LEFT_CW = 13
+LEFT_CCW = 19
+
+# Wheel 2: PWM and output
+RIGHT_PWM = 21
+RIGHT_CW = 16
+RIGHT_CCW = 26
+
+# Left encoder: Input
+LEFT_ENC_A = 8
+LEFT_ENC_B = 7
+
+# Right encoder: Input
+RIGHT_ENC_A = 5
+RIGHT_ENC_B = 6
+
+# Water level sensor: Input
+LEVEL_SENSOR = 12
