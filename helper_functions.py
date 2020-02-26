16e3ac570102c28989050641d51813bc45300828
Chris Oberdorf <cao5292@psu.edu>
2020-02-26 14:59:05 -0500

    Add helper functions file

---
+++ helper_functions.py
@@ -0,0 +1,12 @@
+"""
+Team 2
+Shower Scrubber
+Helper functions for bot code
+
+Contains:
+- set_move_forward : Sets both wheels to forward
+- set_move_reverse : Sets both wheels to reverse
+- set_move_stop    : Stops both wheels
+- set_turn_left    : Turns the bot 90 degrees left
+- set_turn_right   : Turns the bot 90 degrees right
+"""
