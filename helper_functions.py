9a36cdb5f900d3aa4925a12ec0e353444174c69e
Chris Oberdorf <cao5292@psu.edu>
2020-02-26 15:11:40 -0500

    Add move forward function

--- helper_functions.py
+++ helper_functions.py
@@ -10,3 +10,28 @@ Contains:
 - set_turn_left    : Turns the bot 90 degrees left
 - set_turn_right   : Turns the bot 90 degrees right
 """
+
+# Import statements
+import pin_assignments as pins
+import pin_setup
+import RPi.gpio as gpio
+import time
+
+# Set up pins and set motors
+pin_setup.set_io()
+pin_setup.set_pwm()
+
+LEFT_WHEEL = pin_setup.MOTOR_LEFT
+RIGHT_WHEEL = pin_setup.MOTOR_RIGHT
+
+def set_move_forward():
+    # Set left wheel clockwise, right wheel counterclockwise
+    gpio.output(pins.LEFT_CW, True)
+    gpio.output(pins.LEFT_CCW, False)
+    gpio.output(pins.RIGHT_CW, False)
+    gpio.output(pins.RIGHT_CCW, True)
+    
+    # Set both motors running at 100% duty cycle
+    LEFT_WHEEL.ChangeDutyCycle(100)
+    RIGHT_WHEEL.ChangeDutyCycle(100)
+
