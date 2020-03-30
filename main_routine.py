af180458e305a5539bd433f8453015a5804b56c7
Selene Marivaldi <bobdablob42@gmail.com>
2020-03-30 15:56:23 -0400

    Start mapping function

--- main_routine.py
+++ main_routine.py
@@ -8,8 +8,43 @@ Main routine for the bot
 import pin_assignments as pins
 import pin_setup
 import RPi.gpio as gpio
+import helper_functions as movement
 import time
 
 # Set up GPIO
 pin_setup.set_io()
 pin_setup.set_pwm()
+
+# Step completed flags
+map_done = False
+wash_done = False
+rinse_done = False
+
+def mapping_routine():
+    """ Mapping routine for the bot, goes around the perimeter of the shower """
+    num_turns = 0
+    lsw_one, lsw_two, lsw_seven, lsw_eight = False, False, False, False
+    
+    def get_lsw_inputs():
+        lsw_one = gpio.input(pins.LSW_ONE)
+        lsw_two = gpio.input(pins.LSW_TWO)
+        lsw_seven = gpio.input(pins.LSW_SEVEN)
+        lsw_eight = gpio.input(pins.LSW_EIGHT)
+    
+    # Check initial LSW positions, if not placed correctly then abort
+    get_lsw_inputs()
+    if not (lsw_one and lsw_two and lsw_seven and lsw_eight):
+        return
+    
+    # Initial turn 90° to the right
+    movement.set_turn_right()
+    
+    # Move forward until the LSWs are triggered
+    movement.set_move_forward()
+    get_lsw_inputs()
+    
+    while not (lsw_one and lsw_two and lsw_seven and lsw_eight):
+        get_lsw_inputs()
+    else:
+        set_move_stop()
+        
