a574b937a6f66b0e24e06f4ef9a6274bf2bc481a
Selene Marivaldi <bobdablob42@gmail.com>
2020-04-29 23:23:59 -0400

    Add setup to test routine

--- test_routine.py
+++ test_routine.py
@@ -4,4 +4,13 @@ Shower Scrubber
 Testing routine for the bot
 """
 
+# Import statements
+import pin_assignments as pins
+import pin_setup
+import RPi.gpio as gpio
+import helper_functions as movement
+import time
 
+# Set up GPIO
+pin_setup.set_io()
+pin_setup.set_pwm()
