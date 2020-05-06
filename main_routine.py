42993dac007f5d5bef3108d461ddec65610eed43
Selene Marivaldi <bobdablob42@gmail.com>
2020-05-05 22:17:15 -0400

    Change scope of get_lsw_inputs
    It will have more use outside of mapping_routine, so moving it to the entire program would prevent unnecessary rewriting.

--- main_routine.py
+++ main_routine.py
@@ -20,16 +20,18 @@ map_done = False
 wash_done = False
 rinse_done = False
 
+# LSW handling
+lsw_one, lsw_two, lsw_seven, lsw_eight = False, False, False, False
+
+def get_lsw_inputs():
+    lsw_one = gpio.input(pins.LSW_ONE)
+    lsw_two = gpio.input(pins.LSW_TWO)
+    lsw_seven = gpio.input(pins.LSW_SEVEN)
+    lsw_eight = gpio.input(pins.LSW_EIGHT)
+
 def mapping_routine():
     """ Mapping routine for the bot, goes around the perimeter of the shower """
     num_turns = 0
-    lsw_one, lsw_two, lsw_seven, lsw_eight = False, False, False, False
-    
-    def get_lsw_inputs():
-        lsw_one = gpio.input(pins.LSW_ONE)
-        lsw_two = gpio.input(pins.LSW_TWO)
-        lsw_seven = gpio.input(pins.LSW_SEVEN)
-        lsw_eight = gpio.input(pins.LSW_EIGHT)
     
     # Check initial LSW positions, if not placed correctly then abort
     get_lsw_inputs()
